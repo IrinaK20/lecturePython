646300654a8a1320bf136860ddcd135d655e2372
irina <irinakyleashova2519@gmail.com>
2022-12-18 19:44:43 +0300

    1

---
+++ helloPython/hello.py
@@ -0,0 +1,36 @@
+print ('hello')
+
+a=25
+b=1.23
+
+print (a)
+print (b)
+
+value=None
+value=121554
+print (value)
+
+#print(type(a))
+#print(type(b))        #показывает тип 
+#print(type(value))
+
+s='qwerty'
+print(s)    # ввод строковых значений 
+
+#s='hello \nworld'  # \n переходна новую строку 
+#print(s)
+
+print (a,'-', b, '-',s)
+print ('{} - {} - {}'.format(a,b,s))    # способы вывода 
+print (f'{a} - {b} - {s}')
+print ('{2} - {1} - {0}'.format(a,b,s))
+
+
+f=False
+print(f)
+
+list=['1', '6','hello']   #объявление массива 
+print (list)
+
+#ввод и вывод данных 
+# print, input
